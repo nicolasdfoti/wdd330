d7bdc2f2560f1578b8b6ed7081f29e1953202b7f
Nicolás Foti <nicolasdfoti@gmail.com>
2025-05-15 23:45:14 -0300

    Finished 02 Team Activity and 02 Individual Activity

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -24,6 +24,14 @@ export function setClick(selector, callback) {
   qs(selector).addEventListener("click", callback);
 }
 
+
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get(param);
+  return product;
+}
+
 // reusable function to render a list of items using a template function.
 export function renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin", clear = false) {
   
